85088c797ca58d356e63e8e40487399e9c1bc2d6
Jun Kheng <junkheng@gmail.com>
2020-01-07 15:10:33 +0800

    binary search tree

---
+++ Data Structures/ES6/BinarySearchTree.js
@@ -0,0 +1,78 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (value === current.value) return null;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return null;
+    var current = this.root, found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true
+      }
+    }
+    if (!found) return null;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return null;
+    var current = this.root, found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true
+      }
+    }
+    return false;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(14);
+tree.insert(7);
+
+
+console.log(tree.find(5));
