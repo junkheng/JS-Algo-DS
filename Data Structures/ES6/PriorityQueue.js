71128cab4092888c4cabc87dace6bb587b13517c
Jun Kheng <junkheng@gmail.com>
2020-01-08 11:08:10 +0800

    priority queue

---
+++ Data Structures/ES6/PriorityQueue.js
@@ -0,0 +1,81 @@
+// Priority Queue with Min Binary Heap || Insertion and removal O(log N) time complexity, O(N) when searching
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    var newNode = new Node(val, priority)
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();      
+    }
+    return min;
+  }
+  bubbleDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild.priority < element.priority) || (swap !== null && rightChild.priority < leftChild.priority)) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+let ER = new PriorityQueue();
+ER.enqueue("Common Cold", 10);
+ER.enqueue("Slight Fever", 8);
+ER.enqueue("High Fever", 5);
+ER.enqueue("Concussion", 3);
+ER.enqueue("Severed Head", 1);
+console.log(ER);
+ER.dequeue();
+console.log(ER);
+ER.dequeue();
+console.log(ER);
+ER.dequeue();
+console.log(ER);
